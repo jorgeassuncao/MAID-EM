71ef4cccbf9c7b6b75747fc36cc3124d601447ad
Jorge Assunção <jorgeassuncao@users.noreply.github.com>
2020-01-01 18:58:30 +0000

    Pre .gitignore changes

--- code/MAID-EM/MAID-EM.cpp
+++ code/MAID-EM/MAID-EM.cpp
@@ -27,7 +27,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include "RemoteDebug.h"
-#include "../config/userdata_prod.h"
+#include "userdata_marcoervoes.h"
 
 //************* PROJECT AND VERSION **********************************************************************
 //********************************************************************************************************
